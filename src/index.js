a55d534db651e73339d64bb186c826396e56ff4c
Tuomo Kankaanpaa <kankaanpaa.tuomo@gmail.com>
2016-10-26 22:18:49 +0300

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import reducers from './reducers';
+import App from './components/App';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const store = createStoreWithMiddleware(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+, document.getElementById('app'));
